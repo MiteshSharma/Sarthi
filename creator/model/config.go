9205a1a5797c801978d67817dbe9a23e37dc9fac
Mitesh <msharma@kiwiup.com>
2016-09-30 08:50:48 +0530

    Adding database config in config file to be used later

--- creator/model/config.go
+++ creator/model/config.go
@@ -2,14 +2,26 @@ package model
 
 type Config struct  {
 	ServerConfig ServerConfig
+	DatabaseConfig DatabaseConfig
 }
 
 type ServerConfig struct  {
 	Port string
 }
 
+type DatabaseConfig struct  {
+	Host string
+	DbName string
+}
+
 func (o *Config) SaveDefaultConfigParams() {
 	if o.ServerConfig.Port == "" {
 		o.ServerConfig.Port = ":8080"
+	}
+	if o.DatabaseConfig.Host == "" {
+		o.DatabaseConfig.Host = "localhost"
+	}
+	if o.DatabaseConfig.DbName == "" {
+		o.DatabaseConfig.DbName = "sarthi"
 	}
 }
